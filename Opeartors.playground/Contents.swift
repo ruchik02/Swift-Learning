2fba08547b5fe837965e0b4863c457de61104d19
ruchika <ruchikas@geekyants.com>
2024-05-13 14:55:40 +0530

    operators are done

--- Opeartors.playground/Contents.swift
+++ Opeartors.playground/Contents.swift
@@ -1,8 +1,7 @@
 import Foundation
-
+//example-1
 let myAge = 23
 let yourAge = 22
-
 if myAge > yourAge {
     "I'm elder than you"
 } else if myAge < yourAge {
@@ -10,3 +9,26 @@ if myAge > yourAge {
 } else {
     "we are same"
 }
+//example-2 unary prefix
+let foo = !true
+//example-3 unary postfix
+let name = Optional("Ruchika")
+type (of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+let binaryInfix = 1 + 2
+let fullName = "Ruchika" + " " + " Sharma"
+//example-3
+let myMotherAge = myAge + 40
+let doubleMyAge = myAge * 2
+let myFatherAge  = myMotherAge - 10
+//example-4 Ternary
+let age = 30
+//let message: String
+//if age>=18 {
+//    message = "You are an adult"
+//} else {
+//    message = "you are not yet an adult"
+//}
+//message
+let message = age >= 18 ? "You are an adult" : "you are not yet an adult"
